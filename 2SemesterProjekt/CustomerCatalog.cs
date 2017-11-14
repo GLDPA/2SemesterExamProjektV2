4ecdd978e11c2061e9599f61797705241acd10e7
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2017-11-14 14:49:38 +0100

    Customer
    igang med opet kunde:  Gordon

---
+++ 2SemesterProjekt/CustomerCatalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Implementation;
+using DataTransformation.Interfaces;
+using ExtensionsModel.Implementation;
+
+namespace _2SemesterProjekt
+{
+   public class CustomerCatalog : FilePersistableCatalogNoDTO<Customer, CustomerViewModel>
+   {
+       public CustomerCatalog(IFactory<Customer, CustomerViewModel> vmFactory) : base(vmFactory)
+       {
+       }
+   }
+}
