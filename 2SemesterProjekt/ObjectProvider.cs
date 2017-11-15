9c86ff6d1628561514e7068b8557cbb7064a1207
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2017-11-15 13:16:11 +0100

    ObjectProvider

---
+++ 2SemesterProjekt/ObjectProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SemesterProjekt
+{
+    class ObjectProvider
+    {
+        public static SummerHouseCatalog SummerHouseCatalog
+        {
+            get { return SummerHouseCatalog.Instance; }
+        }
+
+        public static CustomerCatalog CustomerCatalog
+        {
+            get { return CustomerCatalog.Instance; }
+        }
+    }
+}
