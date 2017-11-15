7e40675195959dc854b36aa0694a97fc3bad07f1
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2017-11-15 13:44:46 +0100

    Revert "Kunde"
    This reverts commit a3139b06de511d402eee78a4a1ffe86850a3e6e5, reversing
    changes made to 6b1038ce8d0d688b27f84407b84a974350a61622.

--- 2SemesterProjekt/ObjectProvider.cs
+++ 2SemesterProjekt/ObjectProvider.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
 
 namespace _2SemesterProjekt
 {
-    class ObjectProvider
-    {
-        public static SummerHouseCatalog SummerHouseCatalog
-        {
-            get { return SummerHouseCatalog.Instance; }
-        }
-
-        public static CustomerCatalog CustomerCatalog
-        {
-            get { return CustomerCatalog.Instance; }
-        }
-    }
+   //public class ObjectProvider
+   // {
+   //     public static  Domain
+   // }
 }
