64da7810188af53bdb3eecc5f41ddbaa6500edaf
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2017-11-15 11:32:51 +0100

    Stepe 4
    igang med step 4

--- 2SemesterProjekt/CustomerCatalog.cs
+++ 2SemesterProjekt/CustomerCatalog.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Store.Preview.InstallControl;
+using Windows.Graphics.Printing;
 using Catalog.Implementation;
 using DataTransformation.Interfaces;
 using ExtensionsModel.Implementation;
@@ -11,8 +13,24 @@ namespace _2SemesterProjekt
 {
    public class CustomerCatalog : FilePersistableCatalogNoDTO<Customer, CustomerViewModel>
    {
-       public CustomerCatalog(IFactory<Customer, CustomerViewModel> vmFactory) : base(vmFactory)
+       private static CustomerCatalog _instance;
+       
+       public CustomerCatalog(IFactory<Customer, CustomerViewModel> vmFactory) : base(new CustomerViewModelFactory())
        {
        }
+
+       public static CustomerCatalog Instance
+       {
+           get
+            {
+               if (_instance == null)
+               {
+                   _instance = new CustomerCatalog(new CustomerViewModelFactory());
+               }
+               return _instance;
+           } 
+       }
    }
+
+    
 }
