389d7bd1e40b93c8e05776029bfa549aa7ec3a23
mura0149 <31722880+mura0149@users.noreply.github.com>
2017-11-21 14:05:36 +0100

    New Update Faktura

--- 2SemesterProjekt/Faktura/FakturaMasterDetailVm.cs
+++ 2SemesterProjekt/Faktura/FakturaMasterDetailVm.cs
@@ -13,8 +13,8 @@ namespace _2SemesterProjekt.Faktura
         public MasterDetailsViewModel()
            : base(new FakturaVmFactory2(),
                   ObjectProvider.FakturaCatalog,
-                  new List<string> { "Title", "Year", "StudioId" },
-                  new List<string> { "Mins" })
+                  new List<string> { "BookingId", "PhoneNr", "Email", "Price", "CustomerName", "CardNr", "ExpMonth", "ExpYear" },
+                  new List<string> { "CVC" })
         {
 
         }            
