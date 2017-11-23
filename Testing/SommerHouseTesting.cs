99645c0c8749171a86fde4c614f326179c1ba675
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2017-11-23 09:53:56 +0100

    Lavet test projekt

---
+++ Testing/SommerHouseTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class SommerHouseTesting
+    {
+        [TestMethod]
+        public void EmptyFieldTest()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+
+        }
+    }
+}
